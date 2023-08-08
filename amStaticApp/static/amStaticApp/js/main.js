3d38ea3939f2c3db0ee6eb010ff4fd31019a2375
yusufdehbi <63043024+yusufdehbi@users.noreply.github.com>
2023-08-08 20:50:13 +0100

    Resolved styling inconsistencies on the homepage and implemented header scrolling support.

---
+++ amStaticApp/static/amStaticApp/js/main.js
@@ -0,0 +1,34 @@
+
+//Header scrolling 
+const header = document.querySelector('header');
+let navbar_logo = document.querySelector('.header-logo img');
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('navbar-scrolled');
+    navbar_logo.src = "static/images/logo%20-primary%20-transparent%20-horizontal.png";
+    navLinks.forEach(link => 
+        {
+        link.classList.remove('lighten-link')
+        link.classList.add('darken-link')
+
+        }
+    );
+
+
+  } else {
+    header.classList.remove('navbar-scrolled');
+    navbar_logo.src = "static/images/logo%20-white%20-transparent%20-horizontal.png"
+    navLinks.forEach(
+        link => {
+            link.classList.add('lighten-link')
+            link.classList.remove('darken-link')
+        }
+    );
+
+
+
+  }
+}); 
