d91774a1bd9a475e16482f70a90797a54bdac19e
yusufdehbi <63043024+yusufdehbi@users.noreply.github.com>
2023-08-10 09:46:00 +0100

    Resolved styling inconsistencies on the services page

--- amStaticApp/static/amStaticApp/js/main.js
+++ amStaticApp/static/amStaticApp/js/main.js
@@ -9,24 +9,24 @@ window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     header.classList.add('navbar-scrolled');
     navbar_logo.src = "static/images/logo%20-primary%20-transparent%20-horizontal.png";
-    navLinks.forEach(link => 
-        {
-        link.classList.remove('lighten-link')
-        link.classList.add('darken-link')
+    // navLinks.forEach(link => 
+    //     {
+    //     link.classList.remove('lighten-link')
+    //     link.classList.add('darken-link')
 
-        }
-    );
+    //     }
+    // );
 
 
   } else {
     header.classList.remove('navbar-scrolled');
     navbar_logo.src = "static/images/logo%20-white%20-transparent%20-horizontal.png"
-    navLinks.forEach(
-        link => {
-            link.classList.add('lighten-link')
-            link.classList.remove('darken-link')
-        }
-    );
+    // navLinks.forEach(
+    //     link => {
+    //         link.classList.add('lighten-link')
+    //         link.classList.remove('darken-link')
+    //     }
+    // );
 
 
 
